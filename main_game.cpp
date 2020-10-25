f2fda0f220d6ca5d804c91d97b50e97ffd1201a8
SagruH <sagru2899@googlemail.com>
2020-10-25 12:33:48 +0100

    ini

---
+++ main_game.cpp
@@ -0,0 +1,30 @@
+
+//github: https://github.com/SagruH/mastermind_solver
+
+//This file contains the main game logic
+
+
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <vector>
+
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  auto t_start = chrono::high_resolution_clock::now();
+
+
+
+  //code
+
+
+
+  auto t_end = chrono::high_resolution_clock::now();
+  chrono::duration<double> runtime = t_end - t_start; // runtime calc
+  cout << endl << "Runtime: " << runtime.count() << "s\n";
+
+  return 0;
+}
