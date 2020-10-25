b4bccfca9e9367dc670e2c89302cc195e5fe1ab2
SagruH <sagru2899@googlemail.com>
2020-10-25 13:53:11 +0100

    init game and code print

--- main_game.cpp
+++ main_game.cpp
@@ -12,13 +12,37 @@
 
 using namespace std;
 
+class game {
+private:
+  int code_length = 4; //max code length is 16
+  int colour_count = 8;
+  int code[16];
+public:
+  game() {
+    //set code
+    for (size_t i = 0; i < code_length; i++) {
+      code[i] = rand() % colour_count;
+    }
+  }
+
+  void print_code() {
+    for (size_t i = 0; i < code_length; i++) {
+      cout << code[i] << " ";
+    }
+    cout << endl;
+  }
+
+};
+
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
+  srand(time(NULL));
 
-
-
-  //code
+  for (size_t i = 0; i < 50; i++) {
+    game game1;
+    game1.print_code();
+  }
 
 
 
